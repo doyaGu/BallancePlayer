63163d18df30dd2294c817b98a07e16b0431adee
doyaGu <doyaGu@users.noreply.github.com>
2025-04-24 20:50:17 -0400

    Bump version to 0.3.4 and update copyright year to 2025

--- src/config.h
+++ src/config.h
@@ -3,9 +3,9 @@
 
 #define PLAYER_VERSION_MAJOR 0
 #define PLAYER_VERSION_MINOR 3
-#define PLAYER_VERSION_PATCH 3
+#define PLAYER_VERSION_PATCH 4
 #define PLAYER_VERSION_TWEAK 0
-#define PLAYER_VERSION "0.3.3.0"
+#define PLAYER_VERSION "0.3.4.0"
 #define PLAYER_DESCRIPTION "The brand-new player for Ballance"
 
 #define PLAYER_DEFAULT_WIDTH 640
